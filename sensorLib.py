b98ea09918ab04de038db7ab46d9b8f4f6a94719
Masayuki Shimizu <shimizu.masayuki@g.mbox.nagoya-u.ac.jp>
2018-11-03 03:34:43 +0000

    add function for get upLoadStatus and vizualize left_size with gps localization But, without compensate timestamp

--- sensorLib.py
+++ sensorLib.py
@@ -32,3 +32,10 @@ class UploadStatus:
     def __init__(self, operaDB):
         self.opera = operaDB
         self.tables = 'driveragent_uploadstatus'
+
+    def get_DataFrameFromTrip(self, tripLists, l, offset=0):
+        return self.opera.get_DataFromTrip( self.tables, tripLists, l, offset )
+
+    def get_DataFrameFromTime(self, tstart, duration ):
+        return self.opera.get_DataFromTime( self.tables, tstart, duration )
+        
