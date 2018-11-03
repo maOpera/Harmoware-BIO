e97aadca2de537eac04fc1705f26d4ce2ace6d71
Masayuki Shimizu <shimizu.masayuki@g.mbox.nagoya-u.ac.jp>
2018-11-03 03:15:23 +0000

    initially commit example code vizualization of map and gps

--- sensorLib.py
+++ sensorLib.py
@@ -17,6 +17,12 @@ class GPS:
         self.opera = operaDB
         self.tables = 'driveragent_location'
 
+    def get_DataFrameFromTrip(self, tripLists, l, offset=0):
+        return self.opera.get_DataFromTrip( self.tables, tripLists, l, offset )
+
+    def get_DataFrameFromTime(self, tstart, duration ):
+        return self.opera.get_DataFromTime( self.tables, tstart, duration )
+
 class Sensors:
     def __init__(self, operaDB):
         self.opera = operaDB
