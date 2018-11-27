a9ff0f35f48f40ad8ca538846af8ffa47aa409e0
Masayuki Shimizu <shimizu.masayuki@g.mbox.nagoya-u.ac.jp>
2018-11-27 02:57:12 +0000

    add function to get each sensor data in cell phone and example python code

--- sensorLib.py
+++ sensorLib.py
@@ -28,6 +28,56 @@ class Sensors:
         self.opera = operaDB
         self.tables = 'driveragent_sensor'
 
+    def get_ListSensorType(self):
+        print('sensor name : sensor type')
+        print('BMP280 temperature : 65536')
+        print('BMI160 accelerometer : 1')
+        print('BMM150 magnetometer : 2')
+        print('Orientation : 3')
+        print('BMI160 gyroscope : 4')
+        print('TMD27723 Light Sensor : 5')
+        print('BMP280 pressure : 6')
+        print('Gravity : 9')
+        print('Linear Acceleration : 10')
+        print('Rotation Vector : 11')
+        print('BMM150 magnetometer (uncalibrated) : 14')
+        print('Game Rotation Vector : 15')
+        print('BMI160 gyroscope (uncalibrated) : 16')
+        print('Geomagnetic Rotation Vector : 20')
+        print('BMI160 accelerometer (uncalibrated) : 35')
+        print('You need more information, Please see below')
+        print('https://developer.android.com/reference/android/hardware/Sensor')
+
+    def get_DataFrameFromTrip(self, tripLists, l, offset=0):
+        return self.opera.get_DataFromTrip( self.tables, tripLists, l, offset )
+
+    def get_DataFrameFromTime(self, tstart, tend ):
+        return self.opera.get_DataFromTime( self.tables, tstart, tend )
+
+    def get_DataFrameFromTripWithSensorType(self, sensorType, tripLists, l, offset=0):
+        start_datetime, stop_datetime = self.opera.get_TripTime(tripLists, l)
+
+        #Create Query
+        query = 'SELECT * FROM ' + self.tables
+        datetime_conditions = ' WHERE time > ' + str(start_datetime+offset*1000) + ' AND ' + 'time < ' + str(stop_datetime)
+        sensor_opt = ' sensor_type = ' + str(sensorType)
+        query = query + datetime_conditions + ' AND ' + sensor_opt + ';'
+        #print(query)
+
+        column_names, df = self.opera.get_DataFrame( query )
+        return column_names, df
+
+    def get_DataFrameFromTimeWithSensorType(self, sensorType, tstart, tend ):
+        #Create Query
+        query = 'SELECT * FROM ' + self.tables
+        datetime_conditions = ' WHERE time > ' + str(tstart) + ' AND ' + 'time < ' + str(tend)
+        sensor_opt = ' sensor_type = ' + str(sensorType)
+        query = query + datetime_conditions + ' AND ' + sensor_opt + ';'
+        #print(query)
+
+        column_names, df = self.opera.get_DataFrame( query )
+        return column_names, df
+
 class UploadStatus:
     def __init__(self, operaDB):
         self.opera = operaDB
